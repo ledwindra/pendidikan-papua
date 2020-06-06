542a36544557a628e9a0132eec1cd7e411e4ea00
ledwindra <lukman.edwindra@gmail.com>
2020-06-06 19:44:19 +0700

    added/modified/deleted stuffs.

--- run.sh
+++ run.sh
@@ -5,8 +5,11 @@ pip3 install -r requirements.txt
 
 python3 src/get_data.py
 
-jupyter nbconvert index.ipynb --to slides --SlidesExporter.reveal_scroll=True
+jupyter nbconvert index.ipynb --to slides \
+--SlidesExporter.reveal_scroll=True \
+--SlidesExporter.reveal_transition=none
 mv index.slides.html index.html
+
 git add .
 git commit -m "added/modified/deleted stuffs."
 git push origin master
